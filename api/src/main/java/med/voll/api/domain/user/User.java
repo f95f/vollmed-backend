3523f1d3b28ab337ef4cf7b832dea1341d996079
f95f <95felipefernandes@gmail.com>
2024-12-23 02:27:19 -0300

    feat: adiciona tabela e entidade User.

---
+++ api/src/main/java/med/voll/api/domain/user/User.java
@@ -0,0 +1,26 @@
+package med.voll.api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+
+}
