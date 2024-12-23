2ad2a905336f363dcdadb193a273c9086af18581
f95f <95felipefernandes@gmail.com>
2024-12-23 02:08:48 -0300

    feat: add validation for invalid requests.

--- api/src/main/java/med/voll/api/infra/ApiExceptionHandler.java
+++ api/src/main/java/med/voll/api/infra/ApiExceptionHandler.java
@@ -1,10 +1,15 @@
 package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -12,5 +17,20 @@ public class ApiExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> handleNotFound() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ValidationErrorData>> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        var errors = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorData::new).toList());
+    }
+
+
+    private record ValidationErrorData(String field, String message) {
+        public ValidationErrorData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
